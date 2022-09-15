6865a5f4b55f266f9198adbbaddb56c7d492a660
rosemutai <chepngetichrose2030@gmail.com>
2022-09-15 11:40:46 +0300

    configured AOS

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
 import '../styles/globals.css'
+import { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import AOS from 'aos';
+
+// import aos styles
+import 'aos/dist/aos.css';
+// import '../styles/global.css';
+
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // here you can add your aos options
+    AOS.init({
+      easing: "ease-out-cubic",
+      once: true,
+      offset: 50,
+    });
+  }, []);
+  
   return <Component {...pageProps} />
 }
 
