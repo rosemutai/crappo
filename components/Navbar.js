80220914f0843ffe4994eafe86b956f86dee72d5
rosemutai <chepngetichrose2030@gmail.com>
2022-09-15 11:33:36 +0300

    fix links margin

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,27 +2,27 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <header className="navbar md:mx-12 md:flex md:justify-between md:items-center md:h-24">
+    <header className="navbar mx-12 flex flex-col md:flex-row justify-between items-center h-64 md:h-24">
         <div className="logo">
             <h2 className="text-white font-semibold font-inter tracking-widest md:ml-16 ">CRAPPO</h2>
         </div>
 
         <div className="menu">
-            <ul className='menu-links md:flex md:justify-between '>
+            <ul className='menu-links flex flex-col md:flex-row justify-between items-center '>
                 <li>
                     <Link href="/">
-                        <a className='text-white font-light font-rubik md:mr-8'>Products</a>
+                        <a className='text-white font-light font-rubik my-2 md:my-0 md:mr-8'>Products</a>
                     </Link>
                 </li>
 
                  <li>
                     <Link href="/">
-                        <a className='text-white font-light font-rubik md:mr-8'>Features</a>
+                        <a className='text-white font-light font-rubik mb-2 md:mb-0 md:mr-8'>Features</a>
                     </Link>
                 </li>
                  <li>
                     <Link href="/">
-                        <a className='text-white font-light font-rubik md:mr-8'>About</a>
+                        <a className='text-white font-light font-rubik mb-2 md:mb-0 md:mr-8'>About</a>
                     </Link>
                 </li>
                  <li>
@@ -32,15 +32,15 @@ const Navbar = () => {
                 </li>
                  <li>
                     <Link href="/">
-                        <a className='text-white font-light font-rubik md:mr-8'>Login</a>
+                        <a className='text-white font-light font-rubik mb-2 md:mb-0 md:md:mr-8'>Login</a>
                     </Link>
                 </li>
 
-                 <li className='text-grayish font-light font-rubik md:mr-8'> | </li>
+                 <li className='text-grayish font-light font-rubik mb-2 md:mb-0 md:mr-8'> | </li>
 
                  <li>
                     <Link href="/">
-                        <a className='text-white font-light font-rubik bg-lightblue py-2 px-4 rounded-3xl'>Register</a>
+                        <a className='text-white font-light font-rubik mb-2 md:mb-0 bg-lightblue py-2 px-4 rounded-3xl'>Register</a>
                     </Link>
                 </li>
             </ul>
