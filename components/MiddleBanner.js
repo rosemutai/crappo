af0596d0e4d097a054a2e0843033741a5d4f5b03
rosemutai <chepngetichrose2030@gmail.com>
2023-06-01 12:30:41 +0300

    Make page Responsive

--- components/MiddleBanner.js
+++ components/MiddleBanner.js
@@ -6,10 +6,10 @@ import { faPlay} from "@fortawesome/free-solid-svg-icons";
 
 const MiddleBanner = () => {
   return (
-    <div className="middle-banner md:w-10/12  md:mx-auto  md:flex md:justify-between md:items-center md:mt-2">
-        <div className="left-side md:mt-16">
+    <div className="middle-banner md:w-10/12  md:mx-auto  md:flex md:justify-between md:items-center mt-0">
+          <div className="left-side mt-0 md:mt-16 pt-40 md:pt-0">
             <div className="discount-button bg-gray-600 rounded-3xl p-2 flex justify-between items-center
-                w-72 md:w-96 text-sm font-light md:mt-0 mt-12">
+                w-80 md:w-96 text-sm font-light">
                 <p className="smallbutton font-rubik bg-white text-gray-400 rounded-3xl px-2
                     text-center text-sm py-2 md:py-0">70% SAVE</p>
                 <p className="font-rubik text-white">for the best black friday deals</p>
