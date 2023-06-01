70e23e40fba92e048657981b7d8a864d3f6bdc1c
rosemutai <chepngetichrose2030@gmail.com>
2023-06-01 11:27:55 +0300

    Make page Responsive

--- components/MiddleBanner.js
+++ components/MiddleBanner.js
@@ -8,16 +8,16 @@ const MiddleBanner = () => {
   return (
     <div className="middle-banner md:w-10/12  md:mx-auto  md:flex md:justify-between md:items-center md:mt-2">
         <div className="left-side md:mt-16">
-            <div className="discount-button bg-gray-600 rounded-3xl p-2 md:flex md:justify-between
-                md:w-96 text-sm font-light ">
+            <div className="discount-button bg-gray-600 rounded-3xl p-2 flex justify-between items-center
+                w-72 md:w-96 text-sm font-light md:mt-0 mt-12">
                 <p className="smallbutton font-rubik bg-white text-gray-400 rounded-3xl px-2
-                    text-center text-sm">70% SAVE</p>
+                    text-center text-sm py-2 md:py-0">70% SAVE</p>
                 <p className="font-rubik text-white">for the best black friday deals</p>
                 
             </div>
-            <h2 className="ad text-white font-extrabold md:mt-5 text-4xl leading-tight  md:w-96
+            <h2 className="ad text-white font-extrabold mt-12 md:mt-5 text-4xl leading-tight  md:w-96
                 tracking-normal"> Fastest & Secure Platform To Invest In Crypto </h2>
-            <p className="font-rubik font-extralight text-sm text-gray-400 tracking-tight md:my-3">
+            <p className="font-rubik font-extralight text-sm text-gray-400 tracking-wide md:tracking-tight pr-8 md:pr-0 my-6 md:my-3">
                 buy and sell cyptocurrencies by trusted 10M wallets,in over 100m transactions</p>
 
             <button className="font-rubik bg-lightblue md:mt-3  rounded-3xl p-1 text-white w-48
@@ -25,7 +25,7 @@ const MiddleBanner = () => {
                 Try For Free
                 <FontAwesomeIcon
                     icon={faPlay}
-                    className="textxs bg-white text-lightblue w-1.5 h-1.5 px-3 py-2 rounded-3xl md:ml-3"
+                    className="textxs bg-white text-lightblue w-1.5 h-1.5 px-3 py-2 rounded-3xl ml-3"
                 />
             </button>
         </div>
